6d400ef1375e5f56b06942230cd4c7bc87c54b6c
Yuriy S <pushthebo@gmail.com>
2021-11-19 14:59:29 +0300

    more cycles

---
+++ pascal/hello20for.pas
@@ -0,0 +1,7 @@
+program hello20for;
+var
+	x: integer;
+begin
+	for x := 1 to 20 do
+		writeln('Hello, World!');
+end.
---
+++ pascal/square100for.pas
@@ -0,0 +1,8 @@
+program square100;
+var
+	x: integer;
+begin
+	for x := 1 to 99 do
+		write(x*x, ' ');
+	writeln
+end.
