4253988d9e27db5685e62bc98db6d35b5ff17cdc
Yuriy S <pushthebo@gmail.com>
2021-11-22 14:14:18 +0300

    for-loops. diamond

---
+++ pascal/src/diamond.pas
@@ -0,0 +1,32 @@
+program Diamond;
+var
+	n, size, row, col: integer;
+begin
+	repeat
+		write('Enter size of the diamond: ');
+		readln(n)
+	until (n >= 0);
+
+	size := n * 2 + 1;
+
+	for row := 0 to size - 1 do
+	begin
+		for col := 0 to size - 1 do
+		begin
+			if (row <= n) then
+			begin
+				if ((col = n - row) or (col = n + row)) then
+					write('*')
+				else
+					write(' ')
+			end
+			else begin
+				if ((col = row - n) or (col = (size - row + n - 1))) then
+					write('*')
+				else
+					write(' ')
+			end
+		end;
+		writeln()
+	end
+end.
