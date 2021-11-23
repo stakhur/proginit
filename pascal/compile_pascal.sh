6ec8f306bac3fbdbf499b8a732bfe2ffe2c801d0
Yuriy S <pushthebo@gmail.com>
2021-11-23 06:50:30 +0300

    compile script refactored

--- pascal/compile_pascal.sh
+++ pascal/compile_pascal.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+if [ $# -ne 1 ] ; then
+	for s in `ls -1 src/*.pas` ; do $0 $s ; done
+	exit 0
+fi
+
 inp=$1
 name=`echo $inp | awk -F'src/' '{print $2}' | awk -F'.pas' '{print $1}'`
-echo 'NAME: ' $name
 path=`echo $inp | awk -F'src/' '{print $1}'`
-echo 'PATH: ' $path
 
 fpc $inp -FE${path}obj/.
 mv ${path}obj/$name ${path}bin/.
