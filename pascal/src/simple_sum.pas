d9d005a5c0e350c64419c3392bc87baec3ee3925
Yuriy S <pushthebo@gmail.com>
2021-12-04 22:33:47 +0300

    incorrect reading with eof

---
+++ pascal/src/simple_sum.pas
@@ -0,0 +1,13 @@
+program SimpleSum;
+var
+	sum, count, n: longint;
+begin
+	sum := 0;
+	count := 0;
+	while (not eof) do begin
+		read(n);
+		sum := sum + n;
+		count := count + 1
+	end;
+	writeln(count, ' ', sum)
+end.
