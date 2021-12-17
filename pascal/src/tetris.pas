0c805f5036666c7bf7296b53890ab4353a50cd8d
Yuriy S <pushthebo@gmail.com>
2021-12-17 15:24:00 +0300

    show next

--- pascal/src/tetris.pas
+++ pascal/src/tetris.pas
@@ -11,6 +11,12 @@ type
 	Figure = array [1..FigureHeight] of byte;
 	FigurePositions = array [1..NumberOfRotations] of Figure;
 	SetOfFigures = array [1..NumberOfFigures] of FigurePositions;
+	RotateDirection = (RLeft, RRight);
+
+	FigureObject = record
+		id: byte;
+		position: byte
+	end;
 
 const
 	Figures: SetOfFigures = (
@@ -23,8 +29,23 @@ const
 		  (0, 0, 4, 7),		{ +++ }		{ +   }
 		  (0, 3, 2, 2),			{ ++ }		{ +  }
 		  (0, 0, 7, 1),			{  + }		{ +  }
-		  (0, 1, 1, 3))		{  + }		{ ++ }
+		  (0, 1, 1, 3))			{  + }		{ ++ }
 		);
+
+procedure Rotate(var figObj: FigureObject; rotateDir: RotateDirection);
+begin
+	if (rotateDir = RLeft) then begin
+		figObj.position := figObj.position + 1;
+		if (figObj.position > NumberOfRotations) then
+			figObj.position := 1
+	end
+	else if (rotateDir = RRight) then begin
+		figObj.position := figObj.position - 1;
+		if (figObj.position < 1) then
+			figObj.position := NumberOfRotations
+	end
+end;
+
 	
 procedure PrintGlass;
 const
@@ -61,7 +82,41 @@ begin
 	GotoXY(1, 1)
 end;
 
-procedure ShowNext(obj: integer);
+
+procedure PrintFigure(figObj: FigureObject; PosX, PosY: integer);
+var
+	fig: Figure;
+	row, col, curLine: byte;
+	CurX, CurY: integer;
+
+begin
+	{ clear prev figure }
+	CurX := PosX;
+	CurY := PosY;
+	for row := 1 to FigureHeight do begin
+		GotoXY(PosX, CurY + row - 1);
+		for col := 1 to FigureHeight do begin
+			write(' ')
+		end
+	end;
+
+	curLine := 0;
+	fig := Figures[figObj.id][figObj.position]; { #first_row, #second_row, #third_row, #fourth_row }
+	for row := 1 to FigureHeight do begin
+		curLine := fig[row];
+		CurY := PosY + row - 1;
+		for col := 1 to FigureHeight do begin
+			CurX := PosX + col - 1;
+			if ((curLine and (1 << (col - 1))) <> 0) then begin
+				GotoXY(CurX, CurY);
+				write('M');
+			end
+		end
+	end
+end;
+
+
+procedure ShowNext(figObj: FigureObject);
 const
 	NextString = 'Next item';
 
@@ -74,13 +129,25 @@ begin
 
 	GotoXY(Xpos, Ypos);
 	write(NextString);
+	
+	PrintFigure(figObj, Xpos, Ypos + 2);
+
 	GotoXY(1, 1)
 end;
 
+var
+	next: FigureObject;
 begin
+	randomize;
+
 	clrscr;
 	PrintGlass;
-	ShowNext(3);
-	while (not KeyPressed) do;
+	while (not KeyPressed) do begin
+		next.id := random(NumberOfFigures) + 1;
+		next.position := random(NumberOfRotations) + 1;
+		ShowNext(next);
+		delay(500);
+	end;
+
 	clrscr
 end.
