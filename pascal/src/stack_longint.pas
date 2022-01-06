d929c342ba2b9d4ca01f3521aaffe64d368abf57
Yuriy S <pushthebo@gmail.com>
2022-01-06 11:53:42 +0300

    stack

---
+++ pascal/src/stack_longint.pas
@@ -0,0 +1,60 @@
+program StackLongint;
+type
+	LongItemPtr = ^LongItem;
+	LongItem = record
+		data: longint;
+		next: LongItemPtr;
+	end;
+
+	StackOfLongints = LongItemPtr;
+
+procedure SOLInit(var stack: StackOfLongints);
+begin
+	stack := nil
+end;
+
+procedure SOLPush(var stack: StackOfLongints; n: longint);
+var
+	tmp: LongItemPtr;
+begin
+	new(tmp);
+	tmp^.data := n;
+	tmp^.next := stack;
+
+	stack := tmp;
+end;
+
+{ Can not be called for the empty stack }
+procedure SOLPop(var stack: StackOfLongints; var n: longint);
+var
+	tmp:  LongItemPtr;
+begin
+	n := stack^.data;
+
+	tmp := stack;
+	stack := stack^.next;
+	dispose(tmp)
+end;
+
+function SOLIsEmpty(var stack: StackOfLongints): boolean;
+begin
+	SOLIsEmpty := (stack = nil)
+end;
+
+var
+	s: StackOfLongints;
+	n: longint;
+begin
+	SOLInit(s);
+	
+	while not SeekEof do begin
+		read(n);
+		SOLPush(s, n)
+	end;
+
+	while not SOLIsEmpty(s) do begin
+		SOLPop(s, n);
+		write(n, ' ')
+	end;
+	writeln
+end.
